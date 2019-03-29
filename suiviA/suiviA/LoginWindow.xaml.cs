af8db0b5047405247ea2c253d898a5887ac56fa0
Alexandre PREVOT <alexandre.prevot@epsi.fr>
2019-03-29 16:57:47 +0100

    Ajout Login/MainWindow

---
+++ suiviA/suiviA/LoginWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace suiviA
+{
+    /// <summary>
+    /// Logique d'interaction pour LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+        Login login = new Login("Admin", "Admin0", 0);
+
+        private void loginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string user = usernameTextBox.Text;
+            string pass = passwordTextBox.Password;
+            if (login.IsLoggedIn(user, pass))
+            {
+                MessageBox.Show("You are logged in successfully");
+                MainWindow mainWindow = new MainWindow();
+
+                mainWindow.Show();
+                Close();
+            }
+            else
+            {
+                //show default login error message
+                MessageBox.Show("Login Error!");
+            }
+        }
+    }
+}
