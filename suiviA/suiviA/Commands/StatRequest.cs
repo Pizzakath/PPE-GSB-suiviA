afac37a864451014144aa530b6f810399838dda0
Milouz2 <emilien.wacrenier@epsi.fr>
2019-04-05 10:59:52 +0200

    Ajout des statsRequest

---
+++ suiviA/suiviA/Commands/StatRequest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace suiviA.Commands
+{
+    class StatRequest
+    {
+        public string nom { get; set; }
+        public int id { get; set; }
+        public DateTime dateDebut { get; set; }
+        public DateTime dateFin { get; set; }
+
+        public StatRequest()
+        {
+
+        }
+
+        public StatRequest(string nom, int id, DateTime debut)
+        {
+            this.nom = nom;
+            this.id = id;
+            this.dateDebut = dateDebut;
+        }
+
+        public StatRequest(string nom, int id, DateTime dateDebut, DateTime dateFin)
+        {
+            this.nom = nom;
+            this.id = id;
+            this.dateDebut = dateDebut;
+            this.dateFin = dateFin;
+        }
+    }
+}
---
+++ suiviA/suiviA/Commands/StatRequestRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+using Newtonsoft.Json;
+
+namespace suiviA.Commands
+{
+    class StatRequestRepository
+    {
+        private IRestClient restClient;
+
+        public StatRequestRepository()
+        {
+            restClient = ClientRestSharp.GetClient();
+        }
+
+        public int RequeteStat(StatRequest statRequete)
+        {
+            RestRequest req = new RestRequest("api/stats", Method.POST);
+            req.RequestFormat = DataFormat.Json;
+            req.AddJsonBody(JsonConvert.SerializeObject(statRequete));
+            var truc = restClient.Get<int>(req).Data;
+            return truc;
+        }
+    }
+}
